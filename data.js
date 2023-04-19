1914e10b9734f1812139583caefdca32f0ce520d
Shrish Dollin <83923398+shrish-01@users.noreply.github.com>
2023-04-19 21:00:57 +0530

    upload

---
+++ data.js
@@ -0,0 +1,27 @@
+data = [
+    {
+        // 0th id is basically displayed when some network error is there & we are not able to retrive the vehicle data.
+        "id": 0,
+        "title": "",
+        "price": "",
+        "description": "",
+        "brand": "",
+        "image": "#"
+    },
+    {
+        "id": 1,
+        "title": "",
+        "price": "",
+        "description": "",
+        "brand": "",
+        "image": "#"
+    },
+    {
+        "id": 2,
+        "title": "",
+        "price": "",
+        "description": "",
+        "brand": "",
+        "image": "#"
+    },
+]
